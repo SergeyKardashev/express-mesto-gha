dffc6a10d6adcf5f6205008b02c0e350337b7ddd
SergeyKardashev <angeldeep@yandex.ru>
2023-10-17 02:04:18 +0700

    refac: временную мидлвэру вернуть в точку входа для автотестов.

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,14 @@ const usersRouter = require('./usersRouter');
 const cardsRouter = require('./cardsRouter');
 
 // =========== временная мидлвара ===============
-appRouter.use((req, res, next) => {
-  req.user = {
-    // _id: '00000b0b000000000000b00b',
-    _id: '652ba457451ba72e27d7043e',
-  };
-  next();
-});
+// хотел унести из точки входа, но автотесты ругаются.
+// appRouter.use((req, res, next) => {
+//   req.user = {
+//     // _id: '00000b0b000000000000b00b',
+//     _id: '652ba457451ba72e27d7043e',
+//   };
+//   next();
+// });
 
 appRouter.use('/users', usersRouter);
 appRouter.use('/cards', cardsRouter);
