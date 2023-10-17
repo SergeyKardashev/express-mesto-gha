6b0af9f8ac44b49d1949509844db4eaa2eed3aac
SergeyKardashev <angeldeep@yandex.ru>
2023-10-18 00:21:38 +0700

    fix: добавить роут для 404 страницы

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const appRouter = require('express').Router();
 const usersRouter = require('./usersRouter');
 const cardsRouter = require('./cardsRouter');
+const { notFound } = require('../constants/errorCodes');
 
 // =========== временная мидлвара ===============
 // хотел унести из точки входа, но автотесты ругаются.
@@ -11,8 +12,12 @@ const cardsRouter = require('./cardsRouter');
 //   };
 //   next();
 // });
+function wrongUrl(req, res) {
+  return res.status(notFound).send({ message: 'Wrong URL' });
+}
 
 appRouter.use('/users', usersRouter);
 appRouter.use('/cards', cardsRouter);
+appRouter.use('*', wrongUrl);
 
 module.exports = appRouter;
