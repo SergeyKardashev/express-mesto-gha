0e9a1b8cbd23993c82bb15608f71ff2889392417
SergeyKardashev <angeldeep@yandex.ru>
2023-10-22 00:27:14 +0700

    feat: заменить локалхост на IP, сохранить его в переменную окружения, использовать либу validator, отключить versionKey в моделях

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ process.on('uncaughtException', (err, origin) => {
   console.log(`${origin} ${err.name} c текстом ${err.message} не была обработана. Сработал глобальный обработчик ошибок.`);
 });
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 mongoose
-  .connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true })
+  .connect(DB_URL, { useNewUrlParser: true })
   // eslint-disable-next-line no-console
   .then(console.log('MongoDB is connected'));
 
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   req.user = {
     // оставляю невалидный для тестов.
     // _id: '00000b0b000000000000b00b',
-    _id: '652ba457451ba72e27d7043e',
+    _id: '6534054c4a138a25ed61fe45',
   };
   next();
 });
--- models/user.js
+++ models/user.js
@@ -1,22 +1,30 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minlength: [2, 'Минимум 2 символа'],
-    maxlength: [30, 'Максимум более 30 символов'],
-    required: [true, 'Поле должно быть заполнено'],
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      minlength: [2, 'Минимум 2 символа'],
+      maxlength: [30, 'Максимум более 30 символов'],
+      required: [true, 'Поле должно быть заполнено'],
+    },
+    about: {
+      type: String,
+      minlength: [2, 'Минимум 2 символа'],
+      maxlength: [30, 'Максимум более 30 символов'],
+      required: [true, 'Поле должно быть заполнено'],
+    },
+    avatar: {
+      type: String,
+      validate: {
+        validator: (v) => validator.isURL(v),
+        message: 'Некорректный URL',
+      },
+      required: [true, 'Поле должно быть заполнено'],
+    },
   },
-  about: {
-    type: String,
-    minlength: [2, 'Минимум 2 символа'],
-    maxlength: [30, 'Максимум более 30 символов'],
-    required: [true, 'Поле должно быть заполнено'],
-  },
-  avatar: {
-    type: String,
-    required: [true, 'Поле должно быть заполнено'],
-  },
-});
+  { versionKey: false },
+);
 
 module.exports = mongoose.model('user', userSchema);
